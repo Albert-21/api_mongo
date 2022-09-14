22416017a689ef095aec96326ba65d707e75707b
Albert-21 <62444710+Albert-21@users.noreply.github.com>
2022-09-14 17:44:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const app = express();
 // cors
 const cors = require('cors');
 var corsOptions = {
-    origin: '*', // Reemplazar con dominio
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+    origin: '*', 
+    optionsSuccessStatus: 200
 app.use(cors(corsOptions));
 
 // capturar body
